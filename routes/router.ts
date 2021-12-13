952af35dade15943af7dcbe5adab712e79842b34
Fermin <fermin.unicen@gmail.com>
2021-12-13 14:14:08 -0300

    initial commit

---
+++ routes/router.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response } from 'express'
+
+
+const router = Router();
+
+router.get('/mensajes', (req: Request, res: Response) => {
+    res.json({
+        ok: true,
+        msj: 'get listoo'
+    })
+});
+
+router.post('/mensajes', (req: Request, res: Response) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+
+    res.json({
+        ok: true,
+        msj: 'post listo',
+        cuerpo,
+        de
+    })
+});
+
+router.post('/mensajes/:id', (req: Request, res: Response) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        msj: 'post listo',
+        cuerpo,
+        de,
+        id
+    })
+});
+
+
+export default router;
+
